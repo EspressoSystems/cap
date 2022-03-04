09c4cb6bb9f02e959984e386726dc5468eed433c
Charles Lu <charles@espressosys.com>
2022-03-04 17:49:38 -0400

    add GPLv3 license

--- src/testing_apis.rs
+++ src/testing_apis.rs
@@ -1,3 +1,10 @@
+// Copyright (c) 2022 Espresso Systems (espressosys.com)
+// This file is part of the Configurable Asset Privacy (CAP) library.
+
+// This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+// This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+// You should have received a copy of the GNU General Public License along with this program. If not, see <https://www.gnu.org/licenses/>.
+
 //! This file implements various wrappers of internal functions and structs.
 //! It exposes those APIs under `test_apis` feature.
 //! The functions and structs in this file should not be used for other
