8eba194b50f5ca722cd17d59c1f37718802701e5
zhenfei <zhenfei.zhang@hotmail.com>
2022-03-05 10:03:04 -0500

    improve CI

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -3,6 +3,4 @@
 # We want the code to panic if there is an integer overflow
 export RUSTFLAGS="-C overflow-checks=on"
 
-CARGO_HUSKY_DONT_INSTALL_HOOKS=true cargo test --release --no-default-features --features bls12_381 -- -Zunstable-options --report-time
 CARGO_HUSKY_DONT_INSTALL_HOOKS=true cargo test --release --no-default-features --features bn254 -- -Zunstable-options --report-time
-CARGO_HUSKY_DONT_INSTALL_HOOKS=true cargo test --release --no-default-features --features bls12_377 -- -Zunstable-options --report-time
