43956c1af6018412256fb06ec19bddcb643149ae
chancharles92 <chancharles92@users.noreply.github.com>
2022-05-19 18:32:17 +0000

    deploy: 0bb475cd929ad0adf474a48b84c79e2f811be1ca

---
+++ implementors/core/cmp/trait.PartialOrd.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["jf_cap"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.61.0/core/cmp/trait.PartialOrd.html\" title=\"trait core::cmp::PartialOrd\">PartialOrd</a>&lt;<a class=\"struct\" href=\"jf_cap/structs/struct.Amount.html\" title=\"struct jf_cap::structs::Amount\">Amount</a>&gt; for <a class=\"struct\" href=\"jf_cap/structs/struct.Amount.html\" title=\"struct jf_cap::structs::Amount\">Amount</a>","synthetic":false,"types":["jf_cap::structs::Amount"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
---
+++ implementors/core/ops/arith/trait.Sub.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["jf_cap"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.61.0/core/ops/arith/trait.Sub.html\" title=\"trait core::ops::arith::Sub\">Sub</a>&lt;<a class=\"struct\" href=\"jf_cap/structs/struct.Amount.html\" title=\"struct jf_cap::structs::Amount\">Amount</a>&gt; for <a class=\"struct\" href=\"jf_cap/structs/struct.Amount.html\" title=\"struct jf_cap::structs::Amount\">Amount</a>","synthetic":false,"types":["jf_cap::structs::Amount"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
