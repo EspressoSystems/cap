3aa9381fd9a8b34f43f8de647f721aa7886a2716
alxiong <alxiong@users.noreply.github.com>
2022-07-05 04:45:08 +0000

    deploy: 022d1cbc00954695cc010128c25a5765241991da

--- source-files.js
+++ source-files.js
@@ -1,3 +1,3 @@
-var N = null;var sourcesIndex = {};
+var sourcesIndex = {};
 sourcesIndex["jf_cap"] = {"name":"","dirs":[{"name":"bench_utils","files":["mod.rs"]},{"name":"circuit","files":["freeze.rs","gadgets.rs","gadgets_helper.rs","mint.rs","mod.rs","structs.rs","transfer.rs"]},{"name":"proof","files":["freeze.rs","mint.rs","mod.rs","transfer.rs"]},{"name":"utils","files":["mod.rs","params_builder.rs"]}],"files":["constants.rs","errors.rs","freeze.rs","keys.rs","lib.rs","mint.rs","parameters.rs","structs.rs","transfer.rs","types.rs"]};
 createSourceSidebar();
